c8007e337c6ddac6f2634f31db09e37cc2a38e2c
KinguBaka <tarikab92@yahoo.fr>
2022-12-07 10:30:56 +0100

    add goBack + route by id

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/hero';
 import { HeroService } from 'src/app/services/hero.service';
-import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,16 +10,11 @@ import { MessageService } from 'src/app/services/message.service';
 export class HeroesComponent {
   selectedHero?:Hero;
   heroes: Hero[] = [];
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
 
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero:Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
